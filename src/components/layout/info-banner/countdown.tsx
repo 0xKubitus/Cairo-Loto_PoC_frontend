4736dd24b2e037836e9c44ed18769cbe957271ee
0xKubitus <0xKubitus@gmail.com>
2023-10-11 01:17:13 +0200

    countdown implementation in progress -> need to reset the countdown when it reaches 0

--- src/components/layout/info-banner/countdown.tsx
+++ src/components/layout/info-banner/countdown.tsx
@@ -1,12 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Text } from "@chakra-ui/react";
 
 export default function Countdown() {
-  const timerData = "XX d. XX h. XX m.";
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const target = new Date("10/11/2023 03:00:00");
+
+    const interval = setInterval(() => {
+      const now = new Date();
+      const difference = target.getTime() - now.getTime();
+
+      // rounding up the difference between current time and target time in days ->
+      const d = Math.floor(difference / (1000 * 60 * 60 * 24));
+      setDays(d);
+
+      // using modulo to get only the remainder of the difference ->
+      const h = Math.floor(
+        (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      setHours(h);
+      const m = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+      setMinutes(m);
+      const s = Math.floor((difference % (1000 * 60)) / 1000);
+      setSeconds(s);
+    }, 1000); // here "1000" means 1 second, so our timer will refresh every second
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <Text fontSize="xs" color="white">
-      next Lottery Draw in: {timerData}
+      next Lottery Draw in: {days} days, {hours} hours, {minutes} min, {seconds}{" "}
+      sec.
     </Text>
   );
 }
